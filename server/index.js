115a632388de8a4f88a7e0219cac1b4ae387b5c4
King47-code <131360636+King47-code@users.noreply.github.com>
2025-06-26 14:21:41 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,11 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("MotoUber server is running 🚀");
-});
+const authRoutes = require('./routes/auth');
+app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
